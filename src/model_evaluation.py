30d310b28e88693aaff3f9c7377346d138c2bb48
dung <11230522@st.neu.edu.vn>
2025-11-05 19:36:47 +0700

    update tạm

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,9 +1,9 @@
 """
 Model evaluation
-- RMSE, R2, MAPE
+- RMSE, R2, MAE
 """
 
-from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error, mean_absolute_error
 import numpy as np
 import pandas as pd
 
@@ -12,7 +12,6 @@ def evaluate(y_true, y_pred):
     r2 = r2_score(y_true, y_pred)
     mape = np.mean(np.abs((y_true - y_pred)/y_true)) * 100
     return {'RMSE': rmse, 'R2': r2, 'MAPE': mape}
-
 
 def evaluate_multi_output(y_true, y_pred):
     """
@@ -34,36 +33,43 @@ def evaluate_multi_output(y_true, y_pred):
 
     n_outputs = y_true.shape[1]
     results = {
-        "average": {},  # Dành cho các chỉ số trung bình
-        "per_day": {}   # Dành cho các chỉ số của từng ngày
+        "average": {},  # Chỉ số trung bình
+        "per_day": {}   # Chỉ số từng ngày
     }
 
     # --- Phần tính toán chi tiết ---
-    rmse_scores, r2_scores, mape_scores = [], [], []
+    rmse_scores, mae_scores, r2_scores, mape_scores = [], [], [], []
     for i in range(n_outputs):
         day = i + 1
-        # Lấy ra cột tương ứng của ngày thứ i
         true_day_i = y_true[:, i]
         pred_day_i = y_pred[:, i]
 
-        # Tính toán các chỉ số cho ngày thứ i
+        # Tính các chỉ số
         rmse = np.sqrt(mean_squared_error(true_day_i, pred_day_i))
+        mae = mean_absolute_error(true_day_i, pred_day_i)
         r2 = r2_score(true_day_i, pred_day_i)
-        mape = mean_absolute_percentage_error(true_day_i, pred_day_i) * 100 # Dùng hàm của sklearn cho ổn định
+        #mape = mean_absolute_percentage_error(true_day_i, pred_day_i) * 100
 
-        # Lưu vào danh sách để tính trung bình sau
+        # Lưu vào danh sách
         rmse_scores.append(rmse)
+        mae_scores.append(mae)
         r2_scores.append(r2)
-        mape_scores.append(mape)
+        #mape_scores.append(mape)
 
-        # Lưu kết quả chi tiết cho từng ngày
+        # Lưu kết quả chi tiết từng ngày
         results["per_day"][f"RMSE_day_{day}"] = rmse
+        results["per_day"][f"MAE_day_{day}"] = mae
         results["per_day"][f"R2_day_{day}"] = r2
-        results["per_day"][f"MAPE_day_{day}"] = mape
+        #results["per_day"][f"MAPE_day_{day}"] = mape
 
-    # --- Phần tính toán trung bình ---
+    # --- Trung bình ---
     results["average"]["RMSE"] = np.mean(rmse_scores)
+    results["average"]["MAE"] = np.mean(mae_scores)
     results["average"]["R2"] = np.mean(r2_scores)
-    results["average"]["MAPE"] = np.mean(mape_scores)
+    #results["average"]["MAPE"] = np.mean(mape_scores)
 
     return results
+
+
+
+
